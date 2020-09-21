fc7c5934b22d0fad1ca9eeb898de9baa9a282021
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-09-21 16:36:59 +0000

    updated spec and fizzbuzz files

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,26 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-    it 'returns "fizz" when passed 3' do
+    it 'returns "buzz" when passed 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
+    end 
+end
+
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end 
+end
+
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end 
+end
+    
+    
+ describe 'fizzbuzz' do
+    it 'returns "number" when passed other numbers' do
+        expect(fizzbuzz(7)).to eq 7
     end 
 end
